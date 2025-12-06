0cbb2f1d6db93613c066a79f730cb9bf0a48d211
TheKiryuKha <kirillzuyeu@gmai.com>
2025-12-06 16:17:20 +0300

    feat: asking properties

--- src/Commands/MakeEntityCommand.php
+++ src/Commands/MakeEntityCommand.php
@@ -3,6 +3,7 @@
 namespace LaravelRush\Rush\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
 
 class MakeEntityCommand extends Command
 {
@@ -16,15 +17,28 @@ class MakeEntityCommand extends Command
      */
     protected $description = 'Create entity';
 
-    public function handle(): int
+    /**
+     * @return array{type: mixed[][]}
+     */
+    public function handle(): array
     {
-        /**
-         * @var string $name
-         */
-        $name = $this->argument('name');
+        $entity_name = $this->argument('name');
+        $properties = [];
 
-        $this->info("make entity {$name}");
+        while (true) {
+            $property_name = $this->ask('New property name (press <return> to stop adding fields)');
 
-        return 0;
+            if ($property_name === null) {
+                break;
+            }
+
+            $property_type = $this->ask('Field type (enter ? to see all types)', 'string');
+
+            $properties[] = [$property_name => ['type' => $property_type]];
+        }
+
+        Artisan::call("make:model {$entity_name}");
+
+        return $properties;
     }
 }
--- tests/Feature/Feature.php
+++ tests/Feature/Feature.php
@@ -6,7 +6,15 @@ test('first', function (): void {
     $exitCode = Artisan::call('make:entity Todo');
 
     expect($exitCode)->toBe(0);
+});
 
-    $output = Artisan::output();
-    expect($output)->toContain('make entity Todo');
+it("return's correct status code", function (): void {
+
+    $this->artisan('command:name')
+        ->expectsQuestion('New property name (press <return> to stop adding fields)', 'title')
+        ->expectsQuestion('Field type (enter ? to see all types)', 'string')
+        ->expectsQuestion('New property name (press <return> to stop adding fields)', 'description')
+        ->expectsQuestion('Field type (enter ? to see all types)', 'text')
+        ->expectsQuestion('New property name (press <return> to stop adding fields)', null)
+        ->assertExitCode(0);
 });
