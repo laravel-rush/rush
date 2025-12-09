6f59dcf72a3abf1f616721266fffce0294573586
TheKiryuKha <kirillzuyeu@gmai.com>
2025-12-09 18:57:14 +0300

    tests: check's that migrations file has been created

--- tests/Feature/Feature.php
+++ tests/Feature/Feature.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\File;
+
 it("return's correct status code", function (): void {
     $this->artisan('make:entity Todo')
         ->expectsQuestion('New property name (press <return> to stop adding fields)', 'title')
@@ -11,3 +13,19 @@ it("return's correct status code", function (): void {
         ->expectsQuestion('New property name (press <return> to stop adding fields)', null)
         ->assertExitCode(0);
 });
+
+it("create's migrations file", function (): void {
+    $this->artisan('make:entity Todo')
+        ->expectsQuestion('New property name (press <return> to stop adding fields)', 'title')
+        ->expectsQuestion('Field type (enter ? to see all types)', 'string')
+        ->expectsQuestion('New property name (press <return> to stop adding fields)', 'description')
+        ->expectsQuestion('Field type (enter ? to see all types)', 'text')
+        ->expectsQuestion('New property name (press <return> to stop adding fields)', null)
+        ->assertExitCode(0);
+
+    expect(
+        File::exists(database_path('/migrations').'/'.date('Y_m_d_His').'_create_todos_table.php')
+    )->toBeTrue();
+});
+
+// shapshot tests
